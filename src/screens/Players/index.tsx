11a587dcbba3052311efd7d5ebba272473194f25
Mateus Veloso <56977047+mateusvzo@users.noreply.github.com>
2023-10-08 12:11:10 -0300

    UIChanges

---
+++ src/screens/Players/index.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { ButtonIcon } from "@components/ButtonIcon";
+import { Input } from "@components/Input";
+
+import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
+import { Filter } from "@components/Filter";
+
+export function Players() {
+  const [team, setTeam] = useState("Time A");
+  const [players, setPlayers] = useState([])
+
+  return (
+    <Container>
+      <Header showBackButton />
+
+      <Highlight
+        title="Nome da turma"
+        subtitle="adicione a galera e separe os times"
+      />
+
+      <Form>
+        <Input placeholder="Nome da pessoa" autoCorrect={false} />
+        <ButtonIcon icon="add" />
+      </Form>
+
+      <HeaderList>
+        <FlatList
+          data={["Time A", "Time B"]}
+          keyExtractor={(data) => data}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <NumbersOfPlayers>
+          {players.length}
+        </NumbersOfPlayers>
+      </HeaderList>
+    </Container>
+  );
+}
